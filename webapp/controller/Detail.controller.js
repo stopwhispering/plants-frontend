f91d9f0ed24f326e9d2f89ee05490f57b8191116
Johannes <strangewkid@gmail.com>
2019-08-24 20:41:50 +0200

    try

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,482 @@
+sap.ui.define([
+	"plants/tagger/ui/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/Controller",
+	'sap/ui/model/Filter',
+	'sap/ui/model/FilterOperator',
+	'plants/tagger/ui/model/formatter',
+	"sap/base/Log"
+], function (BaseController, JSONModel, Controller, Filter, FilterOperator, formatter, Log) {
+	"use strict";
+	
+	return BaseController.extend("plants.tagger.ui.controller.Detail", {
+		formatter: formatter,
+		onInit: function () {
+			this.oRouter = this.getOwnerComponent().getRouter();
+			this.oModel = this.getOwnerComponent().getModel();
+
+			this.oRouter.getRoute("master").attachPatternMatched(this._onProductMatched, this);
+			this.oRouter.getRoute("detail").attachPatternMatched(this._onProductMatched, this);
+			// this.oRouter.getRoute("untagged").attachPatternMatched(this._onProductMatched, this);
+			
+			this._showFormFragment("Display");
+			
+			// // todo remove
+			// this._count = 1;
+			// window.setInterval(this.onTodoIntervalRemove, 100);
+		},
+		
+		// onTodoIntervalRemove: function(){
+		// 	this._count = this._count + 1;
+		// },
+		
+		oModelPlants: null,
+		
+		filterSubitemsPlants: function(dictsPlants) {
+
+			if ((this.sCurrentPlant === '_untagged photos') && (dictsPlants.length === 0)){
+				return true;
+			}
+			if (this.sCurrentPlant === '_all photos'){
+				return true;
+			}
+			if (this.isDictKeyInArray({key: this.sCurrentPlant}, dictsPlants)){
+				return true;
+			} else {
+				return false;
+			}
+		},
+		
+		handleSuggestPotMaterial: function(evt){
+			// triggered each time user enters something into fragment's pot material input
+			var a = 1;
+			// var sTerm = evt.getParameter("suggestValue")
+		},
+		
+		onIconPressSetPreview: function(evt){
+			// get selected image and current plant in model
+			var sPathCurrentImage = evt.getSource().getBindingContext("images").getPath();
+			var oCurrentImage = this.getOwnerComponent().getModel('images').getProperty(sPathCurrentImage);
+			var sPathCurrentPlant = evt.getSource().getBindingContext("plants").getPath();
+			var oCurrentPlant = this.getOwnerComponent().getModel('plants').getProperty(sPathCurrentPlant);
+			
+			
+			// temporarily set original image as preview image
+			// upon reloading plants model, a specific preview image will be generated 
+			var sUrlOriginal = oCurrentImage['url_original'];
+			var s = JSON.stringify(sUrlOriginal); // model stores backslash unescaped, so we need a workaround
+			var s2 = s.substring(1, s.length-1);
+			oCurrentPlant['url_preview'] = s2;
+			oCurrentPlant['filename_previewimage'] = s2;
+			
+			this.getOwnerComponent().getModel('plants').updateBindings();
+		},
+		
+		applyFilterToListImages: function(sPathCurrentPlant){
+			var oListImages = this.getView().byId('listImages');
+			var oModelPlants = this.getOwnerComponent().getModel('plants');
+
+			this.sCurrentPlant = oModelPlants.getProperty(sPathCurrentPlant).plant_name;
+			
+			// create custom filter function
+			// this.sPathCurrentPlant = sPathCurrentPlant;
+			var oFilter = new sap.ui.model.Filter({
+			    path: 'plants',
+			    test: this.filterSubitemsPlants.bind(this)
+			});
+			
+			var aFilters = [oFilter];
+			var oBinding = oListImages.getBinding("items");
+			oBinding.filter(aFilters);
+		},
+		
+		// onListImagesUpdateStarted: function(evt){
+		// 	if (!this.oBindingContext){
+		// 		return;
+		// 	}
+		// 	var sPathCurrentPlant = this.oBindingContext.getPath();
+		// 	if (sPathCurrentPlant){
+		// 		this.applyFilterToListImages(sPathCurrentPlant);
+		// 	}
+		// },
+		
+		onAfterRendering: function(evt){
+			this.oBindingContext = evt.getSource().getBindingContext("plants");
+		},
+		
+		handleItemPress: function (oEvent) {
+			var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(2),
+				supplierPath = oEvent.getSource().getBindingContext("products").getPath(),
+				supplier = supplierPath.split("/").slice(-1).pop();
+
+			this.oRouter.navTo("detailDetail", {layout: oNextUIState.layout, supplier: supplier});
+		},
+		handleFullScreen: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/fullScreen");
+			this.oRouter.navTo("detail", {layout: sNextLayout, product: this._product});
+		},
+		handleExitFullScreen: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/exitFullScreen");
+			this.oRouter.navTo("detail", {layout: sNextLayout, product: this._product});
+		},
+		handleClose: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/closeColumn");
+			this.oRouter.navTo("master", {layout: sNextLayout});
+		},
+		_onProductMatched: function (oEvent) {
+			this._product = oEvent.getParameter("arguments").product || this._product || "0";
+			this.getView().bindElement({
+				path: "/PlantsCollection/" + this._product,
+				model: "plants"
+			});
+			// remember plants sPath, used in images filtering 
+			var sPathCurrentPlant = "/PlantsCollection/" + this._product;
+			this.applyFilterToListImages(sPathCurrentPlant);
+		},
+		
+		_getPlantModelIndex: function(sPlant, oData){
+			// search for a specific plant in plants model by plant_name 
+			// return the index
+			for (var i = 0; 1 < oData.PlantsCollection.length; i++) {
+			    if(oData.PlantsCollection[i]['plant_name'] === sPlant){
+			    	return i;
+			    }
+			}
+		},
+		
+		onMotherPlantPress: function(evt){
+			var oPlantsModel = this.getOwnerComponent().getModel('plants');
+			var sMotherPlant = evt.getSource().getProperty('title');
+			//find mother plant in model data array
+			var oData = oPlantsModel.getData();
+			var sIndexMotherPlant = this._getPlantModelIndex(sMotherPlant, oData);
+			if (sIndexMotherPlant){
+				//navigate to mother plant in current column
+					var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(1);
+					this.oRouter.navTo("detail", {layout: oNextUIState.layout, product: sIndexMotherPlant});
+			} else {
+				this.handleErrorMessageBox("Can't get Mother Plant Index");
+			}
+		},
+		
+		_toggleButtons: function(bEdit){
+			// this.getView().byId("buttonSave").setVisible(bEdit);
+			this.getView().byId("buttonView").setType(bEdit ? "Transparent" : "Emphasized" );
+			this.getView().byId("buttonEdit").setType(bEdit ? "Emphasized" : "Transparent" );
+			
+			// set the requested form fragment (edit mode or display)
+			this._showFormFragment(bEdit ? "Edit" : "Display");
+		},
+		
+		_formFragments: {},
+		
+		_showFormFragment: function(sFragment){
+			var oContainer = this.byId("objPageSubSection");
+			oContainer.removeAllBlocks();
+			// oContainer.destroyBlocks();
+			
+			// fragment already created?
+			var oFormFragment = this._formFragments[sFragment];
+			if (!oFormFragment){
+				
+				//create fragment
+				oFormFragment = sap.ui.xmlfragment(this.getView().getId(), "plants.tagger.ui.view.Detail" + sFragment);
+				this._formFragments[sFragment] = oFormFragment;
+			}
+			
+			// insert fragment
+			oContainer.addBlock(oFormFragment);
+		},
+		
+		onPressButtonSave: function(evt){
+			// saving images and plants model
+			this.startBusyDialog('Saving...', 'Plants and Images');
+			this.savingPlants = false;
+			this.savingImages = false;
+			
+			// get plants model and identify modified items
+			var oModelPlants = this.getView().getModel('plants');
+			var dDataPlants = oModelPlants.getData();
+			var aModifiedPlants = [];
+			var aOriginalPlants = this.getOwnerComponent().oPlantsDataClone['PlantsCollection'];
+			for (var i = 0; i < dDataPlants['PlantsCollection'].length; i++) { 
+				if (!this.dictsAreEqual(dDataPlants['PlantsCollection'][i], 
+										aOriginalPlants[i])){
+					aModifiedPlants.push(dDataPlants['PlantsCollection'][i]);
+				}
+			}
+			
+			// get images model and identify modified images
+			var oModelImages = this.getView().getModel('images');
+			var dDataImages = oModelImages.getData();
+			
+			var aModifiedImages = [];
+			var aOriginalImages = this.getOwnerComponent().oImagesDataClone['ImagesCollection'];
+			for (i = 0; i < dDataImages['ImagesCollection'].length; i++) { 
+				if (!this.dictsAreEqual(dDataImages['ImagesCollection'][i], 
+										aOriginalImages[i])){
+					aModifiedImages.push(dDataImages['ImagesCollection'][i]);
+				}
+			}		
+			
+			// cancel busydialog if nothing was modified (callbacks not triggered)
+			if((aModifiedPlants.length === 0)&&(aModifiedImages.length === 0)){
+				sap.m.MessageToast.show('Nothing to save.');
+				this.stopBusyDialog();
+				return;
+			}
+			
+			// save plants
+			if(aModifiedPlants.length > 0){
+				this.savingPlants = true;  // required in callback function  to find out if both savings are finished
+				var dPayloadPlants = {'PlantsCollection': aModifiedPlants};
+		    	$.ajax({
+					  url: this.getServiceUrl('/plants_tagger/backend/Plant'),
+					  type: 'POST',
+					  contentType: "application/json",
+					  data: JSON.stringify(dPayloadPlants),
+					  context: this
+					})
+					.done(this.onAjaxSuccessSave)
+					.fail(this.onAjaxFailGeneral);
+			}
+
+			// save images
+			if(aModifiedImages.length > 0){
+				this.savingImages = true;
+				var dPayloadImages = {'ImagesCollection': aModifiedImages};
+		    	$.ajax({
+					  url: this.getServiceUrl('/plants_tagger/backend/Image2'),
+					  type: 'POST',
+					  contentType: "application/json",
+					  data: JSON.stringify(dPayloadImages),
+					  context: this
+					})
+					.done(this.onAjaxSuccessSave)
+					.fail(this.onAjaxFailGeneral);
+			}
+		},
+		
+		onAjaxSuccessSave: function(oMsg, sStatus, oReturnData){
+			
+			// cancel busydialog only neither saving plants nor images is still running
+			if (oMsg.resource === 'PlantResource'){
+				this.savingPlants = false;
+				var oModelPlants = this.getView().getModel('plants');
+				var dDataPlants = oModelPlants.getData();
+				this.getOwnerComponent().oPlantsDataClone = this.getOwnerComponent().getClonedObject(dDataPlants);
+			} else if (oMsg.resource === 'ImageResource'){
+				this.savingImages = false;
+				var oModelImages = this.getView().getModel('images');
+				var dDataImages = oModelImages.getData();
+				this.getOwnerComponent().oImagesDataClone = this.getOwnerComponent().getClonedObject(dDataImages);
+			}
+
+			if(!this.savingPlants&&!this.savingImages){
+				this.stopBusyDialog();
+			}
+		},
+
+		onPressButtonViewMode: function(evt){
+			this._toggleButtons(false);
+		},
+		
+		onPressButtonEditMode: function(evt){
+			this._toggleButtons(true);
+		},
+		
+		plantsValidator: function(args){
+			//todo: used anywhere?
+			var text = args.text;
+			return new sap.m.Token({key: text, text: text});
+		},
+		
+		onInputImageNewPlantNameSubmit: function(evt){
+			// on enter add new plant to image in model
+			var sPlantName = evt.getParameter('value');
+			var dictPlant = {key: sPlantName, 
+							 text: sPlantName};
+			
+			
+			//check if new
+			if(!this.isPlantNameInPlantsModel(sPlantName)){
+				sap.m.MessageToast.show('Plant Name does not exist.');
+				return;
+			}
+			
+			// cancel if emtpy
+			if (sPlantName !== ''){ 
+	
+				//add to model
+				var sPath = evt.getSource().getParent().getBindingContext("images").getPath();
+				var oModel = this.getOwnerComponent().getModel('images');
+				// var oModel = evt.getSource().getModel('images');
+				var aCurrentPlantNames = oModel.getProperty(sPath).plants;
+				
+				// check if already in list
+				if (this.isDictKeyInArray(dictPlant, aCurrentPlantNames)){
+					sap.m.MessageToast.show('Plant Name already assigned. ');
+				} else {
+					oModel.getProperty(sPath).plants.push(dictPlant);
+					Log.info('Assigned plant to image: '+ sPlantName + sPath);
+					oModel.updateBindings();
+				}		
+			}
+			
+			evt.getSource().setValue('');
+		},
+		
+		onInputImageNewKeywordSubmit: function(evt){
+			var sKeyword = evt.getParameter('value');
+			var dictKeyword = {key: sKeyword, 
+							   text: sKeyword
+			};
+			if (!sKeyword){
+				return;
+			}
+			
+			//add to model
+			var sPath = evt.getSource().getParent().getBindingContext("images").getPath();
+			var oModel = this.getOwnerComponent().getModel('images');
+			// var oModel = evt.getSource().getModel('images');
+			oModel.getProperty(sPath).keywords.push(dictKeyword);
+			oModel.updateBindings();
+			
+			evt.getSource().setValue('');
+		},
+		
+		onTokenizerTokenChange: function(evt){
+			if(evt.getParameter('type') === 'removed'){
+				
+				var sType = evt.getSource().data('type'); // plant|keyword
+				
+				var dictRecord = {key: evt.getParameter('token').getProperty('key'), 
+								  text: evt.getParameter('token').getProperty('text')};
+				var sPath = evt.getSource().getParent().getBindingContext("images").getPath();
+				var oModel = this.getOwnerComponent().getModel('images');
+				var aListDicts = sType === 'plant' ? oModel.getProperty(sPath).plants : oModel.getProperty(sPath).keywords;
+				
+				// find dict in array
+				var index = -1;
+				var i;
+				for (i = 0; i < aListDicts.length; i++) { 
+					if(aListDicts[i].key === dictRecord.key){
+						index = i;
+						break;
+					}
+				}
+				// delete
+				if (index >= 0){
+					aListDicts.splice(index, 1);
+				}
+				oModel.updateBindings();
+				
+			}
+			
+		},
+		
+		onPressImagePlantToken: function(evt){
+			// get plant name
+			var sImagePlantPath = evt.getSource().getBindingContext("images").getPath();
+			var sPlant = this.getOwnerComponent().getModel('images').getProperty(sImagePlantPath).key;
+			
+			// get plant path in plants model
+			var oPlantsModel = this.getOwnerComponent().getModel('plants');
+			var oData = oPlantsModel.getData();
+			var iIndexPlant = this._getPlantModelIndex(sPlant, oData);
+			
+			if (iIndexPlant){
+			 	//navigate to plant in layout's current column (i.e. middle column)
+				var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(1);
+				this.oRouter.navTo("detail", {layout: oNextUIState.layout, product: iIndexPlant});
+			 } else {
+			 	this.handleErrorMessageBox("Can't find selected Plant");
+			 }
+		},
+		
+		handleMeasurementPress: function(evt){
+			var a = 1;
+		},
+		
+		_getDialogAddMeasurement : function() {
+			var oView = this.getView();
+			var oDialog = oView.byId('dialogMeasurement');
+			if(!oDialog){
+				oDialog = sap.ui.xmlfragment(oView.getId(), "plants.tagger.ui.view.AddMeasurement", this);
+				oView.addDependent(oDialog);
+			}
+			return oDialog;
+            // if (!this.addMeasurementDialog) {
+            //     this.addMeasurementDialog = sap.ui.xmlfragment("plants.tagger.ui.view.AddMeasurement", this);
+            // }
+            // return this.addMeasurementDialog;
+        },
+        
+    	addMeasurement: function(evt){
+    		// get new data
+       		var oDialog = this._getDialogAddMeasurement();
+			var oModel = oDialog.getModel("new");
+			var dDataNew = oModel.getData();
+			
+			if (!dDataNew['measurement_date']){
+				sap.m.MessageToast.show('Enter date.');
+				return;
+			}
+			
+			// get current measurements in plants model
+			var sPath = evt.getSource().getBindingContext("plants").getPath();
+			var aMeasurements = this.getView().getModel('plants').getProperty(sPath).measurements;
+			if(!aMeasurements){
+				this.getView().getModel('plants').getProperty(sPath)['measurements'] = [];
+				aMeasurements = this.getView().getModel('plants').getProperty(sPath).measurements;
+			}
+			
+			// make sure it's not a duplicate
+			for (var i = 0; i < aMeasurements.length; i++) { 
+				if (aMeasurements[i]['measurement_date'] === dDataNew['measurement_date']){
+		  			sap.m.MessageToast.show('Duplicate (date already exists for this plant).');
+			  		return;
+				}
+			}
+
+			aMeasurements.push(dDataNew);
+			this.getOwnerComponent().getModel('plants').updateBindings();
+			oDialog.close();
+
+        },
+        
+        closeDialogAddMeasurement: function() {
+            this._getDialogAddMeasurement().close();
+		},
+
+		getToday: function(){
+			var today = new Date();
+			var dd = String(today.getDate()).padStart(2, '0');
+			var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+			var yyyy = today.getFullYear();
+			today = yyyy + '-' + mm + '-' + dd;
+			return today;
+		},
+
+		openDialogAddMeasurement: function() {
+			var oDialog = this._getDialogAddMeasurement();
+			var dNewMeasurement = {'plant_name': this.sCurrentPlant,
+								   'measurement_date': this.getToday()
+									};
+			var oPlantsModel = new JSONModel(dNewMeasurement);
+			oDialog.setModel(oPlantsModel, "new");
+			this._getDialogAddMeasurement().open();
+			// var oSimpleForm = this.getView().byId('idSimpleForm');
+			// oSimpleForm.setModel(oPlantsModel);
+			// sap.ui.getCore().byId('idSimpleForm').setModel(oPlantsModel);
+		},
+		
+		onShowUntagged: function(evt){
+			var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(2);
+			this.oRouter.navTo("untagged", {layout: oNextUIState.layout, 
+											product: this._product});
+		}
+		
+
+	});
+}, true);
---
+++ webapp/controller/Master.controller.js
@@ -0,0 +1,278 @@
+sap.ui.define([
+	"plants/tagger/ui/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	'sap/ui/model/Sorter',
+	'sap/m/MessageBox',
+	'plants/tagger/ui/model/formatter'
+], function (BaseController, JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox, formatter) {
+	"use strict";
+
+	return BaseController.extend("plants.tagger.ui.controller.Master", {
+		formatter: formatter,
+		onInit: function () {
+			this.oRouter = this.getOwnerComponent().getRouter();
+			this._bDescendingSort = false;
+			this.createAddDialog();
+		},
+		
+		onAfterRendering: function(){
+			// set number of displayed plants in table title
+			this.updateTableHeaderPlantsCount();	
+		},
+		
+		onListItemPress: function (oEvent) {
+			// if untagged photos layout is open (third column in 3-col-layout), don't change the layout,
+			// otherwise switch to 2-col-layout
+			if (this.getOwnerComponent().getHelper().getCurrentUIState().layout !== "OneColumn"){
+				var oNextUIState = this.getOwnerComponent().getHelper().getCurrentUIState();	
+			} else {
+				oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(1);
+			}
+			
+			// get selected plant
+			var	sPlantPath = oEvent.getSource().getBindingContext("plants").getPath();
+			// "/PlantsCollection/71" --> "71"
+			var	product = sPlantPath.split("/").slice(-1).pop();
+			
+			// use detail (mid-col) route
+			this.oRouter.navTo("detail", {layout: oNextUIState.layout, product: product});
+		},
+		
+		onSearch: function (oEvent) {
+			var sQuery = oEvent.getParameter("query");
+			
+			//check for  filter on active plants
+			var oTableFilterState = [];
+			// var oActiveFilterInfo = this.getView().byId("productsTable").getBinding("items").getFilterInfo();
+			var aActiveFilters = this.getView().byId("productsTable").getBindingInfo('items').binding.aApplicationFilters;
+			
+			//if(aActiveFilters.length && oActiveFilters[0]["sPath"] === "plant_name")
+				//add active-filter to plant_name-filter
+
+			//find out whether we already have a filter on active plants
+			for (var i = 0; i < aActiveFilters.length; i++) {
+			    if (aActiveFilters[i]['sPath'] === "active")
+			    	//remember to delete current active-filter
+			    	var oFilterActive = aActiveFilters[i];
+			}
+			
+			if(oFilterActive){
+				//add filter on active plants
+				oTableFilterState.push(oFilterActive);
+			}
+
+			//add (new) plant_name filter
+			oTableFilterState.push(new Filter("plant_name", FilterOperator.Contains, sQuery));
+
+			//update the aggregation binding's filter
+			this.getView().byId("productsTable").getBinding("items").filter(oTableFilterState, "Application");
+
+			// update count in table header
+			this.updateTableHeaderPlantsCount();
+		},
+		
+		onFilterActive: function(evt){
+			var oTableFilterState = [];
+			//var oActiveFilterInfo = this.getView().byId("productsTable").getBinding("items").getFilterInfo();
+			var aActiveFilters = this.getView().byId("productsTable").getBindingInfo('items').binding.aApplicationFilters;
+			
+			//if(aActiveFilters.length && oActiveFilters[0]["sPath"] === "plant_name")
+				//add active-filter to plant_name-filter
+
+			//find out whether we already have a filter on active or on plant_name
+			for (var i = 0; i < aActiveFilters.length; i++) {
+			    if(aActiveFilters[i]['sPath'] === "plant_name"){
+			    	//remember plant_name filter
+			    	var oFilterPlantName = aActiveFilters[i];
+			    } else if (aActiveFilters[i]['sPath'] === "active")
+			    	//remember to delete current active-filter
+			    	var oFilterActive = aActiveFilters[i];
+			}
+			
+			if(oFilterActive === undefined){
+				//add filter on active plants
+				oTableFilterState.push(new Filter("active", FilterOperator.EQ, true));
+			}
+
+			if(oFilterPlantName){
+				oTableFilterState.push(oFilterPlantName);
+			}
+
+			//update the aggregation binding's filter
+			this.getView().byId("productsTable").getBinding("items").filter(oTableFilterState, "Application");
+			
+			// update count in table header
+			this.updateTableHeaderPlantsCount();
+		},
+
+		updateTableHeaderPlantsCount: function(){
+			// update count in table header
+			var iPlants = this.getView().byId("productsTable").getBinding("items").getLength();
+			var sTitle = "Plants (" + iPlants + ")";
+			this.getView().byId("pageHeadingTitle").setText(sTitle);
+		},
+		
+		onOpenFragmentUploadPhotos: function(oEvent){
+			var oDialog = this._getDialogUploadPhotos();
+			oDialog.open();
+		},
+		
+		closeDialogUploadPhotos: function() {
+            this._getDialogUploadPhotos().close();
+		},
+		
+		uploadPhotosToServer: function(evt){
+			//triggered by upload-button in fragment after selecting files
+			
+			// todo: check files selected
+			var a = 1;
+			
+			var oFileUploader = this.byId("idPhotoUpload");
+			if (!oFileUploader.getValue()) {
+				sap.m.MessageToast.show("Choose a file first");
+				return;
+			}
+			this.startBusyDialog('Uploading...', 'Image File(s)');
+			// set upload url here and not statically to use be able to use service url
+			// todo: implement service url function
+			// var sUrl = this.getServiceUrl("/guys/backend/Photo");
+			var sUrl = 'http://127.0.0.1:5000/plants_tagger/backend/Image';  //automatically POST
+			oFileUploader.setUploadUrl(sUrl);			
+			oFileUploader.upload();
+		},
+		
+		handleTypeMissmatch: function(oEvent) {
+			var aFileTypes = oEvent.getSource().getFileType();
+			jQuery.each(aFileTypes, function(key, value) {aFileTypes[key] = "*." +  value;});
+			var sSupportedFileTypes = aFileTypes.join(", ");
+			sap.m.MessageToast.show("The file type *." + oEvent.getParameter("fileType") +
+									" is not supported. Choose one of the following types: " +
+									sSupportedFileTypes);
+		},
+		
+		handleUploadComplete: function(oEvent){
+			var sResponse = oEvent.getParameter("response");
+			var sMsg;
+			if (sResponse) {
+				var iBegin = sResponse.indexOf("\{");
+				var iEnd = sResponse.indexOf("\}")+1;
+				if (iBegin >= 0 && iEnd >= 0){
+					var sResponseText = sResponse.slice(iBegin, iEnd);
+					var dResponse = JSON.parse(sResponseText);	
+					if (dResponse.hasOwnProperty('error')){
+						sMsg = dResponse.error;
+					} else if (dResponse.hasOwnProperty('success')){
+						sMsg = dResponse.success;
+					} else {
+						sMsg = sResponse;
+					}
+					}
+				else {
+					sMsg = sResponse;
+				}
+
+			} else {
+				// on localhost it seems above doesn't work
+				this.stopBusyDialog();
+				sMsg = "Upload complete, but can't determine status.";
+			}
+			
+			this.stopBusyDialog();
+			sap.m.MessageToast.show(sMsg);
+			this._getDialogUploadPhotos().close();
+		},
+		
+		_getDialogUploadPhotos : function() {
+			var oView = this.getView();
+			var oDialog = this.getView().byId('dialogUploadPhotos');
+			if(!oDialog){
+				oDialog = sap.ui.xmlfragment(oView.getId(), "plants.tagger.ui.view.UploadPhotos", this);
+				oView.addDependent(oDialog);
+			}
+			return oDialog;
+		},
+
+		onAdd: function (oEvent) {
+			//show the add dialog created during init
+			sap.ui.getCore().byId("dialogAdd").open();
+		},
+		
+		createAddDialog: function(){
+			//creates (not shows) add dialog
+			//(called by init)
+			//(will later be identified by id)
+			var oButtonSave = new sap.m.Button("Save", {
+			                    text: "Save",
+			                   tap: [ this.onAddSaveButton, this ]
+			             });
+             var oButtonCancel = new sap.m.Button("Cancel", {
+                    text: "Cancel",
+                    tap: [ this.onAddCancelButton, this ]
+             });
+
+			var oDialog = new sap.m.Dialog("dialogAdd",{
+                    title:"Details of New Entry",
+                    // modal: true,
+                    contentWidth:"1em",
+                    buttons: [ oButtonSave, oButtonCancel ],
+             content:[
+                      new sap.m.Label({text:"Plant Name"}),
+                      new sap.m.Input({
+	                    maxLength: 40,
+	                    id: "inputNewPlantName"
+                      })
+                      ]
+             });	
+		},
+		
+		onAddCancelButton: function(evt){
+			sap.ui.getCore().byId('dialogAdd').close();
+		},
+		
+		onAddSaveButton: function(evt){
+			var sPlantName = sap.ui.getCore().byId("inputNewPlantName").getValue();
+			sap.ui.getCore().byId('dialogAdd').close();
+			
+			//check and not empty
+			if (sPlantName === ''){
+				sap.m.MessageToast.show('Empty not allowed.');
+				return;
+			}
+			
+			//check if new
+			if(this.isPlantNameInPlantsModel(sPlantName)){
+				sap.m.MessageToast.show('Plant Name already exists.');
+				return;
+			}
+			
+			var oModel = this.getOwnerComponent().getModel('plants');
+			var aPlants = oModel.getProperty('/PlantsCollection');
+			var oNewPlant = {'plant_name': sPlantName,
+							 'active': true
+			};
+			aPlants.push(oNewPlant);  // append at end to preserve change tracking with clone 
+			oModel.setProperty('/PlantsCollection', aPlants);
+			// activate changes in controls
+			oModel.updateBindings();
+		},
+
+		onSort: function (oEvent) {
+			// this._bDescendingSort = !this._bDescendingSort;
+			// var oView = this.getView(),
+			// 	oTable = oView.byId("productsTable"),
+			// 	oBinding = oTable.getBinding("items"),
+			// 	oSorter = new Sorter("Name", this._bDescendingSort);
+
+			// oBinding.sort(oSorter);
+			
+			MessageBox.show("This functionality is not ready yet.", {
+				icon: MessageBox.Icon.INFORMATION,
+				title: "Aw, Snap!",
+				actions: [MessageBox.Action.OK]
+			});
+		}
+	});
+}, true);
